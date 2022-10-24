5ad43f6a214c5d87c4a610d0339265eea9071d1b
gvieri <g.vieri@gmail.com>
2022-10-24 13:05:39 +0200

    installation scripts

---
+++ createenvinroment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+chmod a+x Anaconda3-2022.05-Linux-x86_64.sh 
+./Anaconda3-2022.05-Linux-x86_64.sh 
+
+conda create -name testwiththingy
+
+mkdir testwiththingy
+sudo apt install  rtl-433
+sudo apt install  git
+
+cd testwiththingy
+git clone https://github.com/gvieri/AQUACO.git 
+
---
+++ setenvironment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+conda activate testwiththingy 
+cd testwiththingy
+
+conda install pyserial
+conda install numpy
+pip3 install cbor2
+conda install flask-sqlalchemy
+conda install sqlite
+conda install requests
+conda install flask
+
+
+
